38d26a584b78a357b0ccb3973c8723a60449b3b8
Graham84 <graham.osiochain@mycit.ie>
2016-08-18 23:33:58 +0100

    Chapter 2

---
+++ public/dist/test.js
@@ -0,0 +1,13 @@
+'use strict';
+// es6 feature: block-scoped "let" declaration
+
+const sentences = [{ subject: 'cake', verb: 'here', object: 'me now' }, { subject: 'Deer', verb: 'jumping', object: 'muda' }];
+// es6 feature: object destructuring
+function say({ subject, verb, object }) {
+  // es6 feature: template strings
+  console.log(`${ subject } ${ verb } ${ object }`);
+}
+// es6 feature: for..of
+for (let s of sentences) {
+  say(s);
+}
